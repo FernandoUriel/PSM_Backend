84cb8cd45472d95e51d7fd662c63827009914235
Fernando Uriel <fer_uriel20@hotmail.com>
2019-05-21 22:06:37 -0500

    entrega

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.post('/login', function(req, res, next) {
   UserModel.findOne({ username: req.body.username}, (err, user) => {
     if (err) return next(createError(500, 'Error en la base de datos'))
     else if (user) {
+      console.log(user)
       user.isValidPassword(req.body.password, isMatch => {
         if (!isMatch) return next(createError(400, 'Bad Credentials'))
         let options = { issuer: JWT.issuer, audience: JWT.audience }
@@ -22,7 +23,7 @@ router.post('/login', function(req, res, next) {
           exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 10
         }
         let token = jwt.sign(payload, JWT.secretOrKey, options)
-        return res.json({ message: 'ok', token })
+        return res.json({ message: 'ok', token, username:user.username })
       })
     } else return next(createError(400, 'Bad Credentials'))
   })
